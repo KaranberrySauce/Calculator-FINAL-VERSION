68b4d8f25492979bbf6294ecc9b6b41f8ccbbd5a
KaranberrySauce <karan.sidhu98@gmail.com>
2023-10-17 15:47:22 -0700

    Create const variables for all buttons and display

---
+++ script.js
@@ -0,0 +1,9 @@
+const displayHistory = document.querySelector('.history-display');
+const displayInput = document.querySelector('.input-display');
+const displayTemp = document.querySelector('.temp-display');
+
+const numbersElement = document.querySelectorAll('.number');
+const operationElement = document.querySelectorAll('.operation');
+const equalElement = document.querySelector('.equal');
+const allClearElement = document.querySelector('.all-clear');
+const clearLastElement = document.querySelector('.last-entity-clear');
