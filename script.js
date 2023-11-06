99955fa303d06589a278535f7857311c96b29f91
KaranberrySauce <karan.sidhu98@gmail.com>
2023-11-06 13:12:01 -0800

    Clickable and viewable numbers on screen

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const displayHistory = document.querySelector('.history-display');
-const displayInput = document.querySelector('.input-display');
-const displayTemp = document.querySelector('.temp-display');
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector(".display-2");
+const displayTemp = document.querySelector(".temp-result");
 
-const numbersElement = document.querySelectorAll('.number');
-const operationElement = document.querySelectorAll('.operation');
-const equalElement = document.querySelector('.equal');
-const allClearElement = document.querySelector('.all-clear');
-const clearLastElement = document.querySelector('.last-entity-clear');
+const numbersElement = document.querySelectorAll(".number");
+const operationElement = document.querySelectorAll(".operation");
+const equalElement = document.querySelector(".equal");
+const allClearElement = document.querySelector(".all-clear");
+const clearLastElement = document.querySelector(".last-entity-clear");
+
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
+
+numbersElement.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === "." && haveDot) {
+      return;
+    }
+    dis2Num += e.target.innerText;
+    display2El.innerText = dis2Num;
+  });
+});
