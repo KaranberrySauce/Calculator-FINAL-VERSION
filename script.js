5ef55ef9cea36695c7880d15ab6a9f85c5db5edc
KaranberrySauce <karan.sidhu98@gmail.com>
2023-11-06 13:47:34 -0800

    Fully functioning clickable only calculator

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const displayTemp = document.querySelector(".temp-result");
 const numbersElement = document.querySelectorAll(".number");
 const operationElement = document.querySelectorAll(".operation");
 const equalElement = document.querySelector(".equal");
-const allClearElement = document.querySelector(".all-clear");
+const clearAllElement = document.querySelector(".all-clear");
 const clearLastElement = document.querySelector(".last-entity-clear");
 
 let dis1Num = "";
@@ -73,4 +73,18 @@ equalElement.addEventListener("click", (e) => {
   displayTemp.innerText = "";
   dis2Num = result;
   dis1Num = "";
+});
+
+clearAllElement.addEventListener("click", (e) => {
+  display1El.innerText = "0";
+  display2El.innerText = "0";
+  displayTemp.innerText = "0";
+  dis1Num = "";
+  dis2Num = "";
+  result = "";
+});
+
+clearLastElement.addEventListener("click", (e) => {
+  display2El.innerText = "";
+  dis2Num = "";
 });
